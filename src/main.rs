79102b046314549624cbde54d25d8121ecf14620
Connor Szczepaniak <cszczepaniak@gmail.com>
2023-11-12 15:09:10 -0600

    figure out how to get statuses from index to workdir and head to index

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,37 @@
-use git2::StatusOptions;
+use git2::{DiffOptions, StatusOptions};
 
 fn main() {
     println!("Hello, world!");
 
     let repo = git2::Repository::open(".").unwrap();
-    let statuses = repo
+    let d = repo
         .statuses(Some(
             StatusOptions::default()
                 .renames_index_to_workdir(true)
-                .include_untracked(true),
+                .include_untracked(true)
+                .recurse_untracked_dirs(true),
         ))
         .unwrap();
 
-    for status in statuses.iter() {
-        println!("{:?}, {:?}", status.path(), status.status())
+    println!("=== INDEX TO WORKDIR ===");
+
+    for status in d.iter().filter(|st| st.index_to_workdir().is_some()) {
+        println!(
+            "{:?} -> {:?} [{:?}]",
+            status.index_to_workdir().unwrap().old_file().path(),
+            status.index_to_workdir().unwrap().new_file().path(),
+            status.status(),
+        );
+    }
+
+    println!("=== HEAD TO INDEX ===");
+
+    for status in d.iter().filter(|st| st.head_to_index().is_some()) {
+        println!(
+            "{:?} -> {:?} [{:?}]",
+            status.head_to_index().unwrap().old_file().path(),
+            status.head_to_index().unwrap().new_file().path(),
+            status.status(),
+        );
     }
 }
